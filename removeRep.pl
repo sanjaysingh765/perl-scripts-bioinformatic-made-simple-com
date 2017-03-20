ded5ff582c3e850b427a3614c4b71fecb0e205dc
sanjaysingh765 <sanjaysingh765@users.noreply.github.com>
2017-03-19 22:45:18 -0400

    Create removeRep.pl
    Perl script to remove duplicate fasta sequence
    http://www.bioinformatics-made-simple.com/2013/04/how-to-remove-duplicate-sequences-from.html

---
+++ removeRep.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# Downloaded from http://www.bioinformatics-made-simple.com
+
+use Bio::Perl;
+my $infile1 = $ARGV[0];
+my %FIRSTSEQ;
+my $total = 0;
+my $total1=0;
+my $dup = 0;
+
+open (OUTIE, ">$ARGV[1]");
+
+$infile1 = Bio::SeqIO -> new('-format'=>'Fasta',-file => $infile1);
+
+#read in all fasta sequences
+
+while ((my $seqobj = $infile1->next_seq())) 
+{
+
+	$rawid = $seqobj -> display_id;
+	$seq = $seqobj -> seq;
+
+print "ID is $rawid\n";
+#	print "Sequence is $seq\n";
+
+  my $holder = 0;
+
+  if(defined($FIRSTSEQ{$rawid}))
+  {
+		print "Key match with $rawid\n";
+		#$rawid = $rawid.$holder;
+		$dup++;
+	} 
+	else
+	{
+	$FIRSTSEQ{$rawid} = $seq;
+	$total++;
+  }
+}
+
+while ( ($key, $value) = each(%FIRSTSEQ) )
+{
+    print OUTIE ">$key\n";
+		print OUTIE "$value\n\n";
+    $total1++;
+}
+
+print "\n$total unduplicated sequences in the file\n";
+print "$dup duplicated sequences in the file\n";
+print "$total1 unique sequences printed out.\n";
+
+close(OUTIE);
